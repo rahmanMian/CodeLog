7b467b0d3926e8824feccbb7df36a81ce35f5554
Rahman Mian <89672689+rahmanMian@users.noreply.github.com>
2025-01-06 01:53:47 +0500

    Update product-of-array-except-self.java

--- product-of-array-except-self.java
+++ product-of-array-except-self.java
@@ -17,25 +17,22 @@ public int[] productExceptSelf(int[] nums) {
 // prefix postifx -- calculate prodcut for values before and store, calcuata for each step after and store
 // store in two arrays
 // make a final array to store the pre and post mult
-public int[] productExceptSelf(int[] nums) {
-        int n = nums.length;
-        int[] result = new int[n];
-        int[] pre = new int[n];
-        int[] post = new int[n];
 
-        //nothing before so pre is identity element
+    public int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int[] post = new int[n];
+        int[] pre = new int[n];
+        int[] result = new int[n];
+
         pre[0] = 1;
-        ////nothing after so post is identity element
         post[n - 1] = 1;
 
-        //multiply the pre nums value and the array for the prev array for prev valuue
         for(int i = 1; i < n; i++){
-            pre[i] = nums[i-1] * pre[i-1];
+            pre[i] = pre[i - 1] * nums[i - 1];
         }
 
-        //n-2 cuz we need to have a one value gap to multiply
         for(int i = n - 2; i >= 0; i--){
-            post[i] = nums[i+1] * post[i+1];
+            post[i] = post[i + 1] * nums[i + 1];
         }
 
         for(int i = 0; i < n; i++){
@@ -44,3 +41,5 @@ public int[] productExceptSelf(int[] nums) {
 
         return result;
     }
+
+
