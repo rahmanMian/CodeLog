73bfadbf698bc282a81448f2ebd7107271bc8bda
Rahman Mian <89672689+rahmanMian@users.noreply.github.com>
2025-01-12 02:39:13 +0500

    Update and rename twosum.java to two-sum.java

--- two-sum.java
+++ two-sum.java
@@ -1,6 +1,22 @@
 import java.util.HashMap;
 import java.util.Map;
 
+
+//my brute force
+  public int[] twoSum(int[] nums, int target) {
+        
+        for(int i = 0; i < nums.length - 1; i++){
+            for(int j = i + 1; j < nums.length; j++){
+                if (nums[i] + nums[j] == target){
+                    return new int[]{i,j};
+                }
+
+            }
+        }
+        // new int[]{}; works too
+        return null;
+    }
+
 class Solution {
     public int[] twoSum(int[] nums, int target) {
 
