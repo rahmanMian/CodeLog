32ee22fb016903bad9048b889fb7f70bb76d6a53
Rahman Mian <89672689+rahmanMian@users.noreply.github.com>
2025-01-13 22:36:20 +0400

    Update longest-palindromic-substring.java

--- longest-palindromic-substring.java
+++ longest-palindromic-substring.java
@@ -1,3 +1,47 @@
+//BRUTE FORCE
+class Solution {
+    public String longestPalindrome(String s) {
+
+        int maxCount = 0;
+        String maxPalindrome = "";
+
+        
+        for(int i = 0; i < s.length(); i++){
+            //KEEP I+1 AS RIGHT NOT INCLUSIVE
+            for(int j = i + 1; j <= s.length(); j++){
+                String subString = s.substring(i, j);
+               if(isPalindrome(subString) == true){
+                    if(subString.length() > maxCount){
+                        maxPalindrome = subString;
+                        maxCount = subString.length();
+                    }
+               }
+            }
+        }
+        
+        return maxPalindrome;
+    }
+
+    public Boolean isPalindrome(String subString){
+        int left = 0;
+        int right = subString.length() - 1;
+
+
+        if(subString.length() == 1){return true;}
+
+        for(int i = 0; i < subString.length(); i++){
+            if (subString.charAt(left) != subString.charAt(right)){
+                return false;
+            }   
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+}
+
+
 class Solution {
     public String longestPalindrome(String s) {
        if(s.length() <= 1){
