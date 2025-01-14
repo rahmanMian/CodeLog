e7f341cec8b7ac6d36d6b9d1c654c1c5d47d0f99
Rahman Mian <89672689+rahmanMian@users.noreply.github.com>
2025-01-14 19:52:42 +0400

    Update longest-palindromic-substring.java

--- longest-palindromic-substring.java
+++ longest-palindromic-substring.java
@@ -44,24 +44,36 @@ class Solution {
 
 class Solution {
     public String longestPalindrome(String s) {
-       if(s.length() <= 1){
-           return s;
-       }
-           String maxSubStr = "";
-           for(int i = 0; i < s.length() - 1; i++){
-              String odd = subSet(s, i, i);
-            //  String even = subSet(s, i, i + 1);
-            if (odd.length() > maxSubStr.length()) {
-                maxSubStr = odd;
-            }
-           }
-          return maxSubStr;
+       if(s.length() <= 1 ){return s;}
+
+        //WE SEND TWO VALUES AS THERE IS AN EVEN OR ODD CASE FOR EACH SUBSTRING
+        String val1 = subSet(s,0);
+        String val2 = subSet(s,1);
+     return val1.length() > val2.length() ? val1 : val2;
+
+    
     }
-    public String subSet(String s,int left,int right){
-     while(left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)){
-         left--;
-         right++;
-      }
-     return s.substring(left + 1, right);// right isnt inclusive left is(left + 1) right normal
+
+
+    //POINT IS WE HAVE TO FIND A VALUE INSIDE A STRING HENCE WE PULSE FROM THE MIDDLE NOT FROM THE START AND END IN NORMAL PALINDROME
+    public String subSet(String s,int val){
+         String longest = "";
+        for(int i = 0;  i < s.length(); i++){
+           int left = i;
+           int right = i + val;
+           //make sure to keep the index checkes first
+           while( left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)){
+               left--;
+               right++;
+           }
+
+          //the last move in while moves the indexes out of place so we need to shift back in left is inclusive hence +1 while right is not
+           String palindrome = s.substring(left+1,right);
+           if (palindrome.length() >longest.length()){
+               longest = palindrome;
+           }
+        }
+           return longest;
     }
 }
+
