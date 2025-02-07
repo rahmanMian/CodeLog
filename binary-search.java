7629a06c687bb2aa98ea67e683e650cb4a127c15
Rahman Mian <89672689+rahmanMian@users.noreply.github.com>
2025-02-07 13:10:58 +0400

    Create binary-search.java

---
+++ binary-search.java
@@ -0,0 +1,28 @@
+/*
+Binary search very optimal for sorter in ascending order - O(N)
+left <= right is important if the value is on the edge / only value to begin with
+
+
+*/
+
+class Solution {
+    public int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        int mid = 0;;
+
+
+        while(left <= right){
+             mid = (left + right) / 2;
+            if(nums[mid] == target) {
+                return mid;
+            } else if (target < nums[mid]){
+                right = mid - 1;
+            }else{
+                left = mid + 1;
+            }
+        }
+
+        return -1;
+    }
+}
