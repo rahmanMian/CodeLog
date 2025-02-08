0f464124562e4fc5174e99637f0ed391d7ccd171
Rahman Mian <89672689+rahmanMian@users.noreply.github.com>
2025-02-08 18:47:16 +0400

    Update koko-eating-bananas.java

--- koko-eating-bananas.java
+++ koko-eating-bananas.java
@@ -1,1 +1,36 @@
+/*
+  Binary search
+  we need to test every hour with every value to see if the total hours is less than h
+  Math.ceil((double) p / k) imp as it takes a doublle in only
 
+   Arrays.stream(piles).max().getAsInt(); NOT NEED CAN JUST SIMPLY LOOP TRHOUGH
+
+
+*/
+
+
+public class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        int l = 1;
+        //stroes max value of the mile as an in
+        int r = Arrays.stream(piles).max().getAsInt();
+        int res = r;
+
+        while (l <= r) {
+            int k = (l + r) / 2;
+
+            int totalTime = 0;
+            for (int p : piles) {
+                            //needs double and returns double
+                totalTime += Math.ceil((double) p / k);
+            }
+            if (totalTime <= h) {
+                res = k;
+                r = k - 1;
+            } else {
+                l = k + 1;
+            }
+        }
+        return res;
+    }
+}
