78ffe2e95a7a8d0f796470f07ce8a5169b8c5fac
Rahman Mian <89672689+rahmanMian@users.noreply.github.com>
2025-02-11 12:16:14 +0400

    Update search-in-rotated-sorted-array.java

--- search-in-rotated-sorted-array.java
+++ search-in-rotated-sorted-array.java
@@ -2,13 +2,14 @@
 Binary Search here
 Keep your binary search logic strong and think about the solution. This question isn't hard. Once again O(logN)
 REMEMEBER THE YOU CAN ONLY BINARY SEARCH ON SORTED SIDES
-
+We choose by which side is sorted first then check if target is present in it
 
 
 
 */
 
-class Solution {
+
+    class Solution {
     public int search(int[] nums, int target) {
         int left = 0;
         int right = nums.length - 1;
@@ -20,22 +21,22 @@ class Solution {
             if(nums[middle] == target){
                 return middle;
             }
-            
+
             if(nums[left] <= nums[middle]){ //left side is sorted
-                if(target >= nums[left] && target < nums[middle]){ //target is on the left side
-                    right = middle - 1; 
+
+                if(nums[left] <= target && target < nums[middle]){ // if target is on left side
+                    right = middle - 1;
                 }else{
-                    left = middle + 1; //target is on the right of new side
+                    left = middle + 1; //target is not on left side
                 }
-            }else{ //right side sorder
-                if(target <= nums[right] && target > nums[middle]){ //target is on the rig htside
+            }else{ //right side is sorted
+                 if(nums[right] >= target && target > nums[middle]){ //target on right side
                     left = middle + 1; 
                 }else{
-                    right = middle - 1; //target is on the left of new segment side
+                    right = middle - 1; //target not on right side
                 }
             }
         }
-        
         return -1;
-    }
+}
 }
