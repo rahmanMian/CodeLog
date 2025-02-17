5230aef1db726651e19b32ec512f694aad518208
Rahman Mian <89672689+rahmanMian@users.noreply.github.com>
2025-02-17 11:40:20 +0400

    Update longest-substring-with-no-repeating-characters.java

--- longest-substring-with-no-repeating-characters.java
+++ longest-substring-with-no-repeating-characters.java
@@ -1,37 +1,35 @@
+/*
+Sliding window approach - O(N)
+We keep moving till we find a duplicate. How do we find a duplicate. using a hashset.contains().
+keep removing values from the left until the duplicate is removed. Restart the substring.
 
-import java.util.HashSet;
-import java.util.Set;
+
+
+*/
 
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        if (s.length() <= 1) {  //edge case if substring is only one val
-            return s.length();
-        }
-        //use a hashSet-> no duplicates allowed
-        //sliding window approach
 
-        Set<Character> HashSet = new HashSet<>();
-        //this will store our non-dup values
-        int left = 0, max = 0;
-        
-        for(int right = 0; right <  s.length(); right++){
+        Set<Character> set = new HashSet<>();
 
-            if(!HashSet.add(s.charAt(right))){
-               
-                  while (s.charAt(left) != s.charAt(right)) {
-            HashSet.remove(s.charAt(left));
-            left++;
-        }
 
-        left++;
+        int left = 0;
+        int maxSubString = 0;
+
+        for(int right = 0; right < s.length(); right++){
+            char currValue = s.charAt(right);
+
+            while(set.contains(currValue)){
+                set.remove(s.charAt(left));
+                left++;
             }
 
-           
-            max = Math.max(max, (right - left + 1));
+            set.add(currValue);
+
+            maxSubString = Math.max(maxSubString, right - left + 1);
         }
 
-              return max;
-
+        return maxSubString;
         
     }
 }
