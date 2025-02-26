7c681e244c368c940ca1c8971b9eca9763f8408f
Rahman Mian <89672689+rahmanMian@users.noreply.github.com>
2025-02-26 20:48:54 +0400

    Update two-sum.java

--- two-sum.java
+++ two-sum.java
@@ -17,20 +17,27 @@ import java.util.Map;
         return null;
     }
 
-  public int[] twoSum(int[] nums, int target) {
+
+
+//HashMap O(1) lookup time so we run this in O(N)
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
         
-       Map<Integer, Integer> map = new HashMap<>();
+        Map<Integer, Integer> map = new HashMap<>();
 
 
-       for(int i = 0; i < nums.length; i++){
+        for(int i = 0; i < nums.length; i++){
+
             if(map.containsKey(target - nums[i])){
-                return new int[]{map.get(target - nums[i]), i};
+                return new int[]{i, map.get(target - nums[i])};
             }
-            // its put not add
-            map.put(nums[i], i);
-       }
 
-       return new int[]{};
+            map.put(nums[i], i);
+        }
+
+        return new int[]{};
     }
+}
 
 }
