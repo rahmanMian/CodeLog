196d33baf93f41dfa69b1aaa0052cae7f2fa6ebc
Rahman Mian <89672689+rahmanMian@users.noreply.github.com>
2025-03-01 15:38:32 +0400

    Update palindromic-substrings.java

--- palindromic-substrings.java
+++ palindromic-substrings.java
@@ -40,8 +40,39 @@ class Solution {
 EXPAND FROM MIDDLE - O(N^2)
 -EDGE CASE WHEN YOU JUST USE ONE CHAR FROM MIDDLE ONLY ACCOUNTS FOR ODD STRINGS
 -USE TWO VALUES FOR EVEN EDGE CASE
-
-
-
-
+--add count for every expansion
 */
+
+class Solution {
+      int count = 0;
+    public int countSubstrings(String s) {
+
+      
+        for(int i = 0; i < s.length(); i++){
+           
+                isPalindrome(s, i, i);
+                isPalindrome(s, i, i+1);
+          }
+        
+        return count;
+    }
+    
+    
+
+    public boolean isPalindrome(String s, int i ,int j){
+        int left = i;
+        int right = j;
+
+        while(left >= 0 && right < s.length()){
+            if(s.charAt(left) != s.charAt(right)){
+                return false;
+            }
+            count++;
+            left--;
+            right++;
+        }
+        return true;
+    }   
+
+    }
+
