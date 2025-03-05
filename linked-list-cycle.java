1ed067da476fea83718b87a404828d7e74f85bdc
Rahman Mian <89672689+rahmanMian@users.noreply.github.com>
2025-03-05 14:05:16 +0400

    Update linked-list-cycle.java

--- linked-list-cycle.java
+++ linked-list-cycle.java
@@ -11,11 +11,13 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
+
         
         Set<ListNode> seen = new HashSet<>();
 
 
         while(head != null){
+            //each conject has a hashCode which is locatin based. since we are storing objects the .val can be the same
            if(!seen.add(head)){
             return true;
            }
