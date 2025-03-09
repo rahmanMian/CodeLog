f0e383ea452237ecf27d1e7a37145a5c344d931d
Rahman Mian <89672689+rahmanMian@users.noreply.github.com>
2025-03-10 00:21:02 +0400

    Update copy-linked-list-with-random-pointer.java

--- copy-linked-list-with-random-pointer.java
+++ copy-linked-list-with-random-pointer.java
@@ -1,1 +1,44 @@
+/*
+// Definition for a Node.
+class Node {
+    int val;
+    Node next;
+    Node random;
 
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
+*/
+
+/*
+Make HashMap of type node,node
+Loop through list and make deep copy of values
+Loop through list again and reference the new values from there and use the old connections for this list
+
+
+*/
+class Solution {
+    public Node copyRandomList(Node head) {
+        Map<Node, Node> old = new HashMap<>();
+     
+        Node cur = head;
+        while (cur != null){
+            Node copy = new Node(cur.val);
+            old.put(cur, copy);
+            cur = cur.next;
+        }
+
+        cur = head;
+        while (cur != null) {
+            Node copy = old.get(cur);
+            copy.next = old.get(cur.next);
+            copy.random = old.get(cur.random);
+            cur = cur.next;
+        }
+
+        return old.get(head);
+    }
+}
