2c930feea230b7fd02093a33ef9e7d018179c055
Rahman Mian <89672689+rahmanMian@users.noreply.github.com>
2025-03-14 14:17:12 +0400

    Create ind-the-duplicate-number.java

---
+++ ind-the-duplicate-number.java
@@ -0,0 +1,20 @@
+/*
+my sol
+*/
+class Solution {
+    public int findDuplicate(int[] nums) {
+
+        Map<Integer, Integer> map = new HashMap<>();
+        
+
+        for(int num: nums){
+            map.put(num, map.getOrDefault(num, 0) + 1);
+
+            if(map.get(num) > 1){
+                return num;
+            }
+        }
+        
+        return 0;
+    }
+}
