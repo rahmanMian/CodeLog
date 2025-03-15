9f889db1107a55906d7d1f2bb1de1c42fcfd9748
Rahman Mian <89672689+rahmanMian@users.noreply.github.com>
2025-03-15 09:13:40 +0400

    Update best-time-to-buy-and-sell-stock.java

--- best-time-to-buy-and-sell-stock.java
+++ best-time-to-buy-and-sell-stock.java
@@ -6,22 +6,23 @@ keep the left planted until you find a smaller right value. Then shift left to t
 
 class Solution {
     public int maxProfit(int[] prices) {
-        if(prices.length < 2){
-            return 0;
-        }
+      int left = 0;
+      int maxProfit = 0;
 
+        //edge case because of constraint
+      if(prices.length < 2){
+        return 0;
+      }
 
-        int left = 0;
-        int maxProfit = 0;
+      for(int right = 1; right < prices.length; right++){
+         
+         maxProfit = Math.max(maxProfit, prices[right] - prices[left]);
+          
+         if(prices[right] < prices[left]){
+            left = right;
+         }
+      }
 
-        for(int right = 1; right < prices.length; right++){
-            maxProfit = Math.max(prices[right] - prices[left], maxProfit);
-
-            if(prices[right] < prices[left]){
-                left = right;
-            }
-        }
-
-        return maxProfit; 
+      return maxProfit;
     }
 }
