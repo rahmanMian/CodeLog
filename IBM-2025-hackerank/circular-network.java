8d238e54c2f9572ddcf4230657c5d11a52337ac8
Rahman Mian <89672689+rahmanMian@users.noreply.github.com>
2025-03-16 12:56:02 +0400

    Update circular-network.java

--- IBM-2025-hackerank/circular-network.java
+++ IBM-2025-hackerank/circular-network.java
@@ -1,1 +1,18 @@
+/*
+A circular network has n components represented as a string named components. Each component is either a 'Compute' ('C') unit or a 'Network' ('N') unit. The components are arranged in a circle, meaning the first component is adjacent to the last component.
 
+Determine the minimum number of swaps needed to group all the Compute units together, and all the Network units together.
+
+Example
+components = "CNCN"
+
+There are several possible solutions:
+
+Swap indexes 1 and 2 to get 'CCNN'
+Swap indexes 0 and 3 to get "NNCC"
+Swap indexes 0 and 1 to get "NCCN"
+Swap indexes 2 and 3 to get "CNNC"
+The minimum number of moves is 1.
+
+
+*/
