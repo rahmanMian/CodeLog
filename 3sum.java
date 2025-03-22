7163d9b0d252c9bc0934c168fe92798ae2b92074
Rahman Mian <89672689+rahmanMian@users.noreply.github.com>
2025-03-22 16:12:46 +0400

    Update 3sum.java

--- 3sum.java
+++ 3sum.java
@@ -1,43 +1,37 @@
-//MAKE A VISUAL SOL HERE FOR FUTURE
-
-
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-            Arrays.sort(nums);
-
+        
+        //helps avoiding dups
+        Arrays.sort(nums);
         List<List<Integer>> list = new ArrayList<>();
-        for(int i = 0; i < nums.length - 2; i++){
-            //makes check for dups writ ethe way this visually workds
-            if(i > 0 && nums[i] == nums[i-1]){continue;}
+        for(int i = 0; i < nums.length; i++){
+            if(i > 0 && nums[i] == nums[i+1]){continue;}//avoid dups for first value
 
             int start = i + 1;
             int end = nums.length - 1;
-           
+            int sum = nums[i] + nums[start] + nums[end];
             while(start < end){
-                 int sum = nums[i] + nums[start] + nums[end];
 
                 if(sum == 0){
                     list.add(Arrays.asList(nums[i], nums[start], nums[end]));
                 
-                while(start < end && nums[start] == nums[start + 1]){
-                    start++;
+                while(start < end && nums[start] == nums[start + 1]){ //avoiding dups for start pointer
+                    start++; 
                 }
-                while(end > start && nums[end] == nums[end - 1]){
+                while(end > start && nums[end] == nums[end - 1]){//avoiding dups for end pointer
                     end--;
                 }
+
                 start++;
                 end--;
-                //if less tahn zero bring in larger num
-            }else if(sum < 0){
+                }
+            }else if(sum < 0){//since sorter we do this
                 start++;
             }else{
                 end--;
             }
         }     
-        
+        return list;       
                 
-    }
-      return list;     
-
     }
 }
