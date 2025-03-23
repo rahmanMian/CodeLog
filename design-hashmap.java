1ccb931cdf43ec090949b9983f9bd8be1ac16395
Rahman Mian <89672689+rahmanMian@users.noreply.github.com>
2025-03-23 22:38:02 +0400

    Update design-hashmap.java

--- design-hashmap.java
+++ design-hashmap.java
@@ -42,3 +42,24 @@ class MyHashMap {
 
 }
 
+//really mad mem but sick sol
+class MyHashMap {
+    private int[] map;
+    public MyHashMap() {
+        map = new int[10000001];
+        Arrays.fill(map, -1);
+    }
+    
+    public void put(int key, int value) {
+        map[key] = value;
+    }
+    
+    public int get(int key) {
+        return map[key];
+    }
+    
+    public void remove(int key) {
+        map[key] = -1;
+    }
+}
+
