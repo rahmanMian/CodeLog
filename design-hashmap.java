e580ff8991fcddba6a2702a1de9beaa583ef972f
Rahman Mian <89672689+rahmanMian@users.noreply.github.com>
2025-03-23 22:36:31 +0400

    Update design-hashmap.java

--- design-hashmap.java
+++ design-hashmap.java
@@ -1,2 +1,44 @@
-//we have to update existing value
+//NEVER EAEVR USE THIS SOL THIS WAS GOOD PRACTICE -  list.get(i).set(1, value);  HOW TO SET A NEW ARRAY LSIT VALUE
+
+class MyHashMap {
+
+    private List<List<Integer>> list;
+    public MyHashMap() {
+        list = new ArrayList<>();
+    }
+    
+    public void put(int key, int value) {
+        boolean found = false;
+        for(int i = list.size() - 1; i >= 0; i--){
+             if(list.get(i).get(0) == key){
+                list.get(i).set(1, value); 
+                  found = true;
+                break;
+             } 
+           }
+       if (!found) {
+        list.add(new ArrayList<>(Arrays.asList(key, value)));
+    }
+    }
+    
+    public int get(int key) {
+        for(int i = list.size() - 1; i >= 0; i--){
+            if(list.get(i).get(0) == key){
+                return list.get(i).get(1);
+            }
+        }
+        return -1;
+    }  
+    
+    
+    public void remove(int key) {
+         for(int i = 0; i < list.size(); i++){
+            if(list.get(i).get(0) == key){
+                list.remove(i);
+                break;
+            }
+        }
+    }
+
+}
 
