5e9f4456c82c4a2ba09263ece389b3cb4831e537
Rahman Mian <89672689+rahmanMian@users.noreply.github.com>
2025-03-24 21:05:06 +0400

    Create minimum-absolute-difference.java

---
+++ minimum-absolute-difference.java
@@ -0,0 +1,31 @@
+/*
+Only check adjacent we have dsitnc valeues if equal to min that means move on as other values have to be greated since we sorted it
+
+
+
+*/
+
+
+class Solution {
+    public List<List<Integer>> minimumAbsDifference(int[] arr) {
+    
+        Arrays.sort(arr);
+
+        List<List<Integer>> result = new ArrayList<>();
+        int minDiff = Integer.MAX_VALUE;
+
+
+          for (int i = 0; i < arr.length - 1; i++) {
+            minDiff = Math.min(minDiff, arr[i + 1] - arr[i]);
+        }
+
+     
+         for (int i = 0; i < arr.length - 1; i++) {
+            if (arr[i + 1] - arr[i] == minDiff) {
+                result.add(Arrays.asList(arr[i], arr[i + 1]));
+            }
+        }
+
+        return result;
+    }
+}
