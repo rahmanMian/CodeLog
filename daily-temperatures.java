7885e3774ece5dd9d8c2f714fc4fa8dc38724825
Rahman Mian <89672689+rahmanMian@users.noreply.github.com>
2025-04-11 19:55:50 +0400

    Update daily-temperatures.java

--- daily-temperatures.java
+++ daily-temperatures.java
@@ -16,3 +16,24 @@ class Solution {
         return arr;
     }
 }
+
+
+//Stack   - O(2n) -> O(n)
+//stack like pancakes unitl oyu find a value higher - > that means that val is higher than all the values in the stack. You can now empty the stack one by one and get the days till
+
+
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        Stack<Integer> stack = new Stack<>();
+        int[] days =  new int[temperatures.length];
+
+        for(int i = 0; i < temperatures.length ; i++){
+            while(!stack.isEmpty() && temperatures[stack.peek()] < temperatures[i]){
+                 days[stack.peek()] = i - stack.pop();
+            }
+            stack.push(i);
+        }
+
+        return days;
+      
+    }
