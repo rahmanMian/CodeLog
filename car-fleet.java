d6261468e5589a32613426a53b5b6df4edb3448a
Rahman Mian <89672689+rahmanMian@users.noreply.github.com>
2025-04-13 15:59:07 +0400

    Update car-fleet.java

--- car-fleet.java
+++ car-fleet.java
@@ -3,9 +3,8 @@
 Why double -> time can be integer
 Why hashMap -> we have to sort position and keeo links with speed
 Why stack -> values will merge and move forwared
-Why descending -> Values closer to target might finish -> starting from the back will be more complicated. You will have to keep account of more variables
+Why descending -> IT IS A ONE LANE STREET. WHAT IS THE REASON OF TRAFFIC? -> THE CARS AT THE FRONT. THEY WILL STOP THE CARS AT THE BACK -> HENCE IT WOULD BE WRONG TO  CACLUCATE FROM THE START
 Why time -> (target - position[i]) /  speed[i]) -> simple takes longer wont become fleet vice versa
-
 O(nlogn) -> Dual-Pivot Quicksort -> O(n) < O(nlogn) -> we take the worse time
  
 */
