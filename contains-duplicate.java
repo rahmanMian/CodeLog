8bca8af4baea569e374e7fab7bb40d251b946c54
Rahman Mian <89672689+rahmanMian@users.noreply.github.com>
2025-08-08 00:57:48 +0400

    we back

--- contains-duplicate.java
+++ contains-duplicate.java
@@ -8,3 +8,19 @@ class Solution {
        return false;
     }
 }
+
+
+// // we back
+// //class Solution {
+//     public boolean containsDuplicate(int[] nums) {
+//         Set<Integer> set = new HashSet<>();
+
+//         for(int i = 0; i < nums.length; i++){
+//             if(!set.add(nums[i])){
+//                 return true;
+//             }
+
+//         }
+//         return false;
+//     }
+// } 
