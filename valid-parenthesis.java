173b4ef73368fa3ea3a82e929b4b7c87bb27f77b
Rahman Mian <89672689+rahmanMian@users.noreply.github.com>
2025-08-29 07:19:09 +0400

    Update valid-parenthesis.java

--- valid-parenthesis.java
+++ valid-parenthesis.java
@@ -1,3 +1,5 @@
+//add the other brakcer and we have to remove it for the next line
+
 class Solution {
     public boolean isValid(String s) {
         Stack<Character> stack = new Stack<>();
@@ -11,7 +13,7 @@ class Solution {
                 stack.push(']');
             } else {
                 // If it's a closing bracket, check if stack is empty or if the popped value doesn't match
-                //isEmpty for  edge case: "]"
+                //isEmpty for  edge case: "]" 
                 if (stack.isEmpty() || stack.pop() != val) {
                     return false;
                 }
