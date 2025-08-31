ca632dec2f07d26e11a2349502f9bff5df12d583
Rahman Mian <89672689+rahmanMian@users.noreply.github.com>
2025-08-31 05:07:42 +0400

    Update evaluate-reverse-polish-notation.java

--- evaluate-reverse-polish-notation.java
+++ evaluate-reverse-polish-notation.java
@@ -5,10 +5,13 @@ class Solution {
         Stack<Integer> stack = new Stack<>();
         for(String token: tokens){
            if( !(token.equals("+") || token.equals("-") || token.equals("*") || token.equals("/"))){
+            //Integer.parseInt(token) remember the syntac
             stack.push(Integer.parseInt(token));
            }else{
             int val2 = stack.pop();
             int val1 = stack.pop();
+            ///.equals for strings
+               
             if(token.equals("/")){
                 stack.push(val1 / val2);
             }else if (token.equals("+")){
