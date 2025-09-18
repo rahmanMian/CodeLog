bf10603f092365e7d6a9bf9d30e2193255e495b3
Rahman Mian <89672689+rahmanMian@users.noreply.github.com>
2025-09-17 20:46:16 -0400

    Update longest-substring-with-no-repeating-characters.java

--- longest-substring-with-no-repeating-characters.java
+++ longest-substring-with-no-repeating-characters.java
@@ -9,23 +9,26 @@ If you dont use contains it reduces time by 2 ms
 
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-
+        //set up 
         Set<Character> set = new HashSet<>();
 
-
+        
         int left = 0;
         int maxSubString = 0;
-
+        // move right poitner
         for(int right = 0; right < s.length(); right++){
+            // pick char
             char currValue = s.charAt(right);
-
+            // if set has star contains dup - start removing till you find
             while(set.contains(currValue)){
+                //remove
                 set.remove(s.charAt(left));
+                // mvove left
                 left++;
             }
-
+            // add value back in
             set.add(currValue);
-
+            // r - l cuz if both same char we need + 1
             maxSubString = Math.max(maxSubString, right - left + 1);
         }
 
