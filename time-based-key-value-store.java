756b50716bdd9987b3542d9a78bb796ff7262e9d
Rahman Mian <89672689+rahmanMian@users.noreply.github.com>
2025-09-25 17:52:26 -0400

    Update time-based-key-value-store.java

--- time-based-key-value-store.java
+++ time-based-key-value-store.java
@@ -15,6 +15,23 @@ map.get(key).add(new Data(value, timestamp)); //geting key adding new value
                 left = middle + 1;
 
 
+Design a time-based key-value data structure that can store multiple values for the same key at different time stamps and retrieve the key's value at a certain timestamp.
+
+Implement the TimeMap class:
+
+TimeMap() Initializes the object of the data structure.
+void set(String key, String value, int timestamp) Stores the key key with the value value at the given time timestamp.
+String get(String key, int timestamp) Returns a value such that set was called previously, with timestamp_prev <= timestamp. If there are multiple such values, it returns the value associated with the largest timestamp_prev. If there are no values, it returns "".
+ 
+
+Example 1:
+
+Input
+["TimeMap", "set", "get", "get", "set", "get", "get"]
+[[], ["foo", "bar", 1], ["foo", 1], ["foo", 3], ["foo", "bar2", 4], ["foo", 4], ["foo", 5]]
+Output
+[null, null, "bar", "bar", null, "bar2", "bar2"]
+
 */
 
 
