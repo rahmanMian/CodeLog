b171d52cc2a5821df5d4d80ee5e5559e68e0c176
Rahman Mian <89672689+rahmanMian@users.noreply.github.com>
2025-09-27 23:22:53 -0400

    Update koko-eating-bananas.java

--- koko-eating-bananas.java
+++ koko-eating-bananas.java
@@ -17,23 +17,31 @@ public class Solution {
        int right = 0;
        int k = 0;
 
+       // loop through pile and find max pile
+       // find max pile  value
        for(int pile: piles){
           right = Math.max(pile, right);
        }
 
+
+
+      // use bonary search to make it faster
        while(left <= right){
         int middle = (left + right) / 2;
         int hoursTaken = 0;
        
-
+        // now we test for every k from min 0 to  max right 
         for(int pile: piles){
             hoursTaken += Math.ceil((double) pile / middle);   // pile + (pile + middle - 1) / middle
         }
 
+        // if its less than h make that the middle 
+         // test values lower tahn it 
         if(hoursTaken <= h){
             k = middle;
             right = middle - 1;
         }else{
+            // if greated we are going to slow move the left one above
             left = middle + 1;
         }
        }
