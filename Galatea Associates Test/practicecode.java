437eb320a6a2d3a65e2888e5987cfd7f6db8fa46
Rahman Mian <89672689+rahmanMian@users.noreply.github.com>
2025-10-23 22:05:10 -0400

    Update practicecode.java

--- Galatea Associates Test/practicecode.java
+++ Galatea Associates Test/practicecode.java
@@ -1,6 +1,11 @@
 // KK COM 100
-// KK SOV 110
+// KKK SOV 110
 // KKK CoM 120
+// 200 from KK COM
+//60% of COM LEFT
+//80% OF KKK LEFT
+// 960 OF kkk com
+//1160
 
 mp=1000
 
@@ -8,3 +13,7 @@ mp=1000
 //
 //80% KKK
 //80% COM
+
+
+  //Option one just full sendy the last stop
+  //1200 total you owq
