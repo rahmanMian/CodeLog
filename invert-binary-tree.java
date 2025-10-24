19fc0aff836ff3c25529bb279e1740634dddceb6
Rahman Mian <89672689+rahmanMian@users.noreply.github.com>
2025-10-23 22:11:40 -0400

    Define TreeNode and stub invertTree method
    Added a class definition for a binary tree node and a method stub for inverting a binary tree.

---
+++ invert-binary-tree.java
@@ -0,0 +1,27 @@
+i/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+//spli st fork and swap
+// make two funcs
+
+
+
+
+ */
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        
+    }
+}
