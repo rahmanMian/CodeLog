f5cfc10dff26d21616eaedd6bf7b6f520917c919
Rahman Mian <89672689+rahmanMian@users.noreply.github.com>
2025-10-24 23:38:44 -0400

    Add invertTree method to invert binary tree
    Implemented a method to invert a binary tree using recursion.

--- invert-binary-tree.java
+++ invert-binary-tree.java
@@ -1,4 +1,4 @@
-i/**
+/**
  * Definition for a binary tree node.
  * public class TreeNode {
  *     int val;
@@ -17,11 +17,29 @@ i/**
 // make two funcs
 
 
-
-
- */
 class Solution {
     public TreeNode invertTree(TreeNode root) {
-        
-    }
+        TreeNode rootCopy = root;
+
+        if(root == null){return root;}
+  
+         traverse(root);
+          
+          return root;
+
+        }
+
+ 
+public void traverse(TreeNode node){
+    if(node == null){return;}
+
+    TreeNode temp = node.left;
+    node.left = node.right;
+    node.right = temp;
+
+    traverse(node.left);
+    traverse(node.right);
+   
+}
+
 }
