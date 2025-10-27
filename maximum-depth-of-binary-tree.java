63b0762b958f7cc0c503f2c8c51b33c8bc6c77c8
Rahman Mian <89672689+rahmanMian@users.noreply.github.com>
2025-10-26 20:41:05 -0400

    Define TreeNode and initialize maxDepth method
    Added definition for TreeNode structure and initial maxDepth method.

--- maximum-depth-of-binary-tree.java
+++ maximum-depth-of-binary-tree.java
@@ -1,1 +1,18 @@
-
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+//send the traverse in with the count as well, keep a public static int maxCount 
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        
+    }
+};
