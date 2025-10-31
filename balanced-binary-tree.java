f24bcb95b220cd33fbedf4c7fe8ac522450ad04a
Rahman Mian <89672689+rahmanMian@users.noreply.github.com>
2025-10-31 18:26:08 -0400

    Create balanced-binary-tree.java

---
+++ balanced-binary-tree.java
@@ -0,0 +1,17 @@
+ /* not workign */
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        if(root == null)return true;
+
+        if(Math.abs(getHeight(root.left) - getHeight(root.right) ) > 1){
+            return false;
+        }else{
+            return true;
+        }
+        
+    }
+public int getHeight(TreeNode node) {
+    if (node == null) return 0;
+    return 1 + Math.max(getHeight(node.left), getHeight(node.right));
+}
+}
