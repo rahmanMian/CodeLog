18205b1efa42a82125f8bf9c924b78743b681190
Rahman Mian <89672689+rahmanMian@users.noreply.github.com>
2025-11-02 16:44:12 -0500

    Refactor comments and add TreeNode class definition
    Updated comments to clarify the definition of a balanced binary tree and added a TreeNode class definition.

--- balanced-binary-tree.java
+++ balanced-binary-tree.java
@@ -1,4 +1,30 @@
- /* not workign */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ *}
+ */
+/* Lets get this straih -> what is a balanceed tree -> a tree whoses forks have a difference of dept by one max -> i made the mistakete
+
+          1
+        /   \
+       2     2
+      /       \
+     3         3
+    /           \
+   4             4
+   THIS IS THE CASE WHY YOU HAVE TO CHECK EVERY NODE
+*/
+
 class Solution {
     public boolean isBalanced(TreeNode root) {
         if(root == null)return true;
@@ -13,4 +39,5 @@ class Solution {
 public int getHeight(TreeNode node) {
     if (node == null) return 0;
     return 1 + Math.max(getHeight(node.left), getHeight(node.right));
+}
 }
