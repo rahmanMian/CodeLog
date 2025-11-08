e47a69067142d72843eb725742679f8d25eb3ae8
Rahman Mian <89672689+rahmanMian@users.noreply.github.com>
2025-11-08 18:50:07 -0500

    Update maximum-depth-of-binary-tree.java

--- maximum-depth-of-binary-tree.java
+++ maximum-depth-of-binary-tree.java
@@ -13,28 +13,44 @@
  *     }
  * }
  */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
 
     public static int maxCount;
 
     public int maxDepth(TreeNode root) {
-        //keep maxCount here cuz its not supposed to be shared between test cases
-        maxCount = 0;
-        if(root == null){ return 0;}
 
-       traverse(root, 1); //1 cuz root is 1
+        maxCount = 0;
+        
+        traverse(root);
+        return maxCount;
+    }
+
+
+    public int traverse(TreeNode node){
+        if(node == null){ return 0;}
+    
+
+       int leftDept =  1  + traverse(node.left);
+       int rightDept = 1 + traverse(node.right);
+
+
+       maxCount = Math.max(leftDept, rightDept);
+
        return maxCount;
     }
-
-
-    public void traverse(TreeNode node, int count){
-
-                if(node == null){return;}
-                
-                maxCount = Math.max(count, maxCount);
-                traverse(node.left, count + 1);
-                traverse(node.right, count + 1);
-    }
-
-
 }
