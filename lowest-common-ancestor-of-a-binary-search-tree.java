f4ec97b00f225cef489fdad668bb0005eb848f9c
Rahman Mian <89672689+rahmanMian@users.noreply.github.com>
2025-11-13 20:22:28 -0500

    Create lowest-common-ancestor-of-a-binary-search-tree.java

---
+++ lowest-common-ancestor-of-a-binary-search-tree.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+ /*
+Given a BST find the Lowest common node. > For two nodes p and q find the common descneant above them T. Look at exmplamep 2 for edge case
+
+My first thouhg woulld be pick and one of them and try traversing backwards to the other node. You then store the lca >
+2 cases if you direclty hit the node from p or q first that will be lc
+The pivot point will be the lca
+
+
+
+
+
+
+
+ */
+
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        
+    }
+}
