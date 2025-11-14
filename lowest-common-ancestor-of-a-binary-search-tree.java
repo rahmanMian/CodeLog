b08cbf3f91d9730f95d99feb373610752bf72ce2
Rahman Mian <89672689+rahmanMian@users.noreply.github.com>
2025-11-14 17:59:12 -0500

    Enhance comments for lowestCommonAncestor method
    Added detailed comments and examples for the lowestCommonAncestor method.

--- lowest-common-ancestor-of-a-binary-search-tree.java
+++ lowest-common-ancestor-of-a-binary-search-tree.java
@@ -10,12 +10,49 @@
  /*
 Given a BST find the Lowest common node. > For two nodes p and q find the common descneant above them T. Look at exmplamep 2 for edge case
 
-My first thouhg woulld be pick and one of them and try traversing backwards to the other node. You then store the lca >
-2 cases if you direclty hit the node from p or q first that will be lc
-The pivot point will be the lca
 
 
 
+
+			6
+	               / \
+                      4   9
+
+P = 4 AND Q = 9
+
+start with P
+is p less than Q? Yes
+Does P have a root? Yes
+No lets move up -> p.root
+is node 6 less than Q? Yes
+Does Node 6 have a root?No
+Move down
+Is node 9? Yes 
+
+
+P = 1 and Q = 9
+
+Root case
+start with P
+is P less than Q? Yes
+move up > p.root
+is node 3 less than Q? Yes
+move up > p.root.root
+is node 6 less than Q? Yes
+null(no root) store value
+return value
+
+P = 1 and Q = 2
+is p less than Q? Yes
+move up > p.root
+is node 3 less than Q? No
+return node 3
+
+		6
+	       / \
+              3   7
+	     / \   \
+            
 
 
 
