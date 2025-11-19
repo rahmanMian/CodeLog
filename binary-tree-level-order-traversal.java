cef88af77fa21b85618ddb3510748994e3d2a353
Rahman Mian <89672689+rahmanMian@users.noreply.github.com>
2025-11-19 15:53:19 -0500

    Update binary-tree-level-order-traversal.java

--- binary-tree-level-order-traversal.java
+++ binary-tree-level-order-traversal.java
@@ -32,45 +32,68 @@ Send one right store its value
 What is the case for a level to be compleate?
 Think of the recursion case to sweep a level
  */
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+ /*
+Return the level order traversal of the binary tree 
+1. Left to Right
+2. Level to Level
+Call Root
+Have helper function to traverse for nodes
+HaveAnother helper to store values
+Send one left store its value
+Send one right store its value
+
+   1
+  2  3
+    4  5
+
+
+
+What is the case for a level to be compleate?
+ */
 class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
-        //List is an interface not a class -> you cannot do new List<>();
-        List<List<Integer>> list = new ArrayList<>();
-    if(root != null){
-        list.add(Arrays.asList(root.val));
-        levelSweeper(root.left, root.right, list);
+        //make the arraylist we will send back
+        List<List<Integer>> result = new ArrayList<>();
+        //if root is null return result instantly
+        if (root == null) return result;
+        //Start with the root value as a list and also send the main list
+        sweepLevel(Arrays.asList(root), result);
+        return result;
     }
-        return list;
-    }
 
-    public void levelSweeper(TreeNode leftNode, TreeNode rightNode, List<List<Integer>> list){
-        List<Integer> li = new ArrayList<>();
-          
-
-          if (leftNode != null) {
-            if(leftNode.left != null){
-              li.add(leftNode.left.val);
-            }
-            if(leftNode.right != null){
-              li.add(leftNode.right.val);
-            }
-        }
-             if (rightNode != null) {
-            if(rightNode.left != null){
-              li.add(rightNode.left.val);
-            }
-            if(rightNode.right != null){
-              li.add(rightNode.right.val);
-            }
-        }
-
-
-        if(!li.isEmpty()){list.add(li);}
+    private void sweepLevel(List<TreeNode> levelNodes, List<List<Integer>> result) {
+        if (levelNodes.isEmpty()) return;    // <-- base case
         
-        levelSweeper(leftNode.left, rightNode.right, list);
+        // list of treenodes for next level
+        List<TreeNode> next = new ArrayList<>();
+        //storing all the values to be returned in the lists value
+        List<Integer> values = new ArrayList<>();
+        
+        // store all the values on that level 
+        for (TreeNode node : levelNodes) {
+            values.add(node.val);
+            if (node.left != null) next.add(node.left);
+            if (node.right != null) next.add(node.right);
+        }
 
+        result.add(values);
+        sweepLevel(next, result);
     }
-
 }
 
-}
